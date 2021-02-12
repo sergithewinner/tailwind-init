9e403fb35d66f87f4a81f1b86f3f3ca437bc6e47
sergithewinner <sergithewinner@gmail.com>
2021-02-12 14:16:20 +0400

    OK

--- setup.sh
+++ setup.sh
@@ -5,19 +5,19 @@ curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt install jq -y 
 sudo apt install moreutils -y
+sudo npm i -g live-server
 mkdir -p project/dist/ && mkdir project/src/
 cd project/dist/ && touch index.html style.css
 cd ../src/ && touch style.css
 cd ../ && npm init -y
 npm install tailwindcss@latest postcss@latest autoprefixer@latest && npx tailwindcss init
+npm install watch
+printf '@tailwind base;\n@tailwind components;\n@tailwind utilities;' >> src/style.css
 jq '.scripts.test = "tailwind build src/style.css -o dist/style.css"' package.json | sponge package.json
 sed -i s/test/build:css/g package.json
-printf '@tailwind base;\n@tailwind components;\n@tailwind utilities;' >> src/style.css
-npm run build:css
-touch .gitignore && echo "node_modules/" >> .gitignore && git rm -r --cached node_modules ; git status
-sudo npm i -g live-server
-live-server dist/ 
-npm install watch
 jq '.scripts += {watch: "watch npm run build:css ./src"}' package.json | sponge package.json
 sed -i s/"npm run build:css"/"'npm run build:css'"/g package.json
+touch .gitignore && echo "node_modules/" >> .gitignore && git rm -r --cached node_modules ; git status
 npm run watch
+live-server dist/ 
+
