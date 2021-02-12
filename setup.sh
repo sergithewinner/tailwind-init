d6d2ede42701a4009411189fa5e77e2f9eef90f8
sergithewinner <sergithewinner@gmail.com>
2021-02-12 15:08:56 +0400

    OK

--- setup.sh
+++
@@ -1,8 +0,0 @@
-cd ~
-sudo apt update && sudo apt upgrade -y
-sudo apt install git -y
-mkdir -p work/TailwindCSS-$(date +"%d-%m-%Y")
-cd work/TailwindCSS-$(date +"%d-%m-%Y")
-git clone https://github.com/sergithewinner/tailwind-init.git
-cd tailwind-init/
-code . && bash install.sh
